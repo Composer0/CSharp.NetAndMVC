4625adafc3d6df17f77924e4efa6dd4d9ca2b778
Orion Palmer <opalmer1989@gmail.com>
2022-12-12 11:16:55 -0500

    Updated Models needed for Mortgage Calculator

--- PalindromeChecker/PalindromeChecker/Models/LoanModel.cs
+++ PalindromeChecker/PalindromeChecker/Models/LoanModel.cs
@@ -1,10 +1,21 @@
-﻿namespace PalindromeChecker.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//namespace must be present in order for models to be represented/instantiated in another model.
+namespace PalindromeChecker.Models
 {
     public class Loan
     {
-        public int Price { get; set; }
-        public int Rate { get; set; }
-        public int LoanAmount { get; set; }
+        public decimal Payment { get; set; }
+        public decimal Rate { get; set; }
+        public decimal LoanAmount { get; set; }
+        public decimal TotalInterest { get; set; }
+        public decimal TotalCost { get; set; }
+        public int Term { get; set; }
         public string Message { get; set; }
+
+        public List<LoanPayment> Payments { get; set; } = new List<LoanPayment>();
     }
 }
