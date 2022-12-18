d084f37309e8abc039b438f3c42fb0db815f94e2
Orion Palmer <opalmer1989@gmail.com>
2022-12-18 10:12:29 -0500

    ContactPro w/ PostgreSQL
    - create .net 6 project
    - setup PostgreSQL connection
    - create a new AppUser model using inheritance
    - add FirstName & LastName properties
    - enforce a maximum and minimum lengths
    - add a NotMapped property
    - add a new migration and update the database

---
+++ ContactPro/ContactPro/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ContactPro.Models;
+
+namespace ContactPro.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser> //We added the type <AppUser> to the original so that it will know to use that model.
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ ContactPro/ContactPro/Models/AppUser.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+// Allows us to markup classes. Affects the way that the communications are processed.
+using Microsoft.AspNetCore.Identity;
+
+namespace ContactPro.Models
+{
+    public class AppUser : IdentityUser //inherits from IdentityUser into our new class.
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and a max {1} characters long.", MinimumLength = 2)] //Max Length followed by error followed by Minimum Length.
+        public string? FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and a max {1} characters long.", MinimumLength = 2)] //0 = The Name 2 = the second property min length 1 = the first property max length.
+        public string? LastName { get; set; }
+
+        [NotMapped]
+        public string? FullName { get { return $"{FirstName}{LastName}"; } } // Similar to JavaScript $`... interpolates into document.
+    }
+}
