9e0fabb1f2ecf69bdaa4a6d72b5be5c508212cf2
Orion Palmer <opalmer1989@gmail.com>
2022-12-28 10:15:15 -0500

    Update Create Category
    - remove AppUserId on view. Made sure it still sent through the controller.

--- ContactPro/ContactPro/Controllers/CategoriesController.cs
+++ ContactPro/ContactPro/Controllers/CategoriesController.cs
@@ -64,7 +64,6 @@ namespace ContactPro.Controllers
         [Authorize]
         public IActionResult Create()
         {
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id");
             return View();
         }
 
@@ -75,13 +74,15 @@ namespace ContactPro.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,AppUserId,Name")] Category category)
         {
+            ModelState.Remove("AppUserId");
             if (ModelState.IsValid)
             {
+                string appUserId = _userManager.GetUserId(User);
+                category.AppUserId = appUserId;
                 _context.Add(category);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", category.AppUserId);
             return View(category);
         }
 
